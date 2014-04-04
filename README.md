bdb63532676eb62a4d71bbd8892160b3bb4fa037
Rob Halff <rob.halff@gmail.com>
2014-04-04 19:43:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Websocket for Chiχ
+==========
---
+++ nodes/client/node.js
@@ -0,0 +1,35 @@
+var client = new websocket.client;
+output = function() {
+
+  client.on('connectFailed', function(error) {
+    cb({ error: error });
+  });
+
+  client.on('connect', function(connection) {
+
+    connection.on('error', function(error) {
+      cb({ error: error });
+    });
+
+    connection.on('close', function() {
+      cb({ close: null });
+    });
+
+    connection.on('close', function() {
+      cb({ close: null });
+    });
+
+    connection.on('message', function(message) {
+      cb({ message: message });
+    });
+
+    cb({
+      connection: connection,
+      client: client
+    });
+
+  });
+
+  client.connect(input.url, input.protocol);
+
+};
