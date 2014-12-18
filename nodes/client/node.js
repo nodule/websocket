262ee8c46dc40cea8cd5b1081c5f1eb427038963
Rob Halff <rob.halff@gmail.com>
2014-12-18 05:26:50 +0100

    update

--- nodes/client/node.js
+++ nodes/client/node.js
@@ -1,35 +1,34 @@
-on.input.url = function() {
+on.input.send = function(data) {
 
-  state.client = null;
+  if(!state.client) {
 
-  if(input.protocol) {
-    state.client = new websocket.w3cwebsocket(input.url, input.protocol);
-  } else {
-    state.client = new websocket.w3cwebsocket(input.url);
+    if(input.protocol) {
+      state.client = new websocket.w3cwebsocket(input.url, input.protocol);
+    } else {
+      state.client = new websocket.w3cwebsocket(input.url);
+    }
+
+    state.client.onmessage = function(event) {
+      output({ message: JSON.parse(event.data) });
+    };
+
+    state.client.onerror = function(event) {
+      output({ error: event });
+    };
+
+    state.client.onclose = function(event) {
+      output({ close: event });
+    };
+
+    state.client.onopen = function(event) {
+      output({
+        client: state.client,
+        open: event
+      });
+    };
+
   }
 
-  state.client.onmessage = function(event) {
-    output({ message: JSON.parse(event.data) });
-  };
-
-  state.client.onerror = function(event) {
-    output({ error: event });
-  };
-
-  state.client.onclose = function(event) {
-    output({ close: event });
-  };
-
-  state.client.onopen = function(event) {
-    output({
-      client: state.client,
-      open: event
-    });
-  };
-
-};
-
-on.input.send = function(data) {
   if(state.client && state.client.readyState === state.client.OPEN) {
     state.client.send(JSON.stringify(data));
   } else {
