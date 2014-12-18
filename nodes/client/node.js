2910341178fb97a44fc1a6bca45e66d368fcd200
Rob Halff <rob.halff@gmail.com>
2014-12-18 05:38:02 +0100

    async

--- nodes/client/node.js
+++ nodes/client/node.js
@@ -1,33 +1,33 @@
-on.input.send = function(data) {
+on.input.url = function(url) {
 
-  if(!state.client) {
-
-    if(input.protocol) {
-      state.client = new websocket.w3cwebsocket(input.url, input.protocol);
-    } else {
-      state.client = new websocket.w3cwebsocket(input.url);
-    }
-
-    state.client.onmessage = function(event) {
-      output({ message: JSON.parse(event.data) });
-    };
-
-    state.client.onerror = function(event) {
-      output({ error: event });
-    };
-
-    state.client.onclose = function(event) {
-      output({ close: event });
-    };
-
-    state.client.onopen = function(event) {
-      output({
-        client: state.client,
-        open: event
-      });
-    };
-
+  if(input.protocol) {
+    state.client = new websocket.w3cwebsocket(url, input.protocol);
+  } else {
+    state.client = new websocket.w3cwebsocket(url);
   }
+
+  state.client.onmessage = function(event) {
+    output({ message: JSON.parse(event.data) });
+  };
+
+  state.client.onerror = function(event) {
+    output({ error: event });
+  };
+
+  state.client.onclose = function(event) {
+    output({ close: event });
+  };
+
+  state.client.onopen = function(event) {
+    output({
+      client: state.client,
+      open: event
+    });
+  };
+
+};
+
+on.input.send = function(data) {
 
   if(state.client && state.client.readyState === state.client.OPEN) {
     state.client.send(JSON.stringify(data));
