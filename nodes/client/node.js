33925a09d9bba036243c4e2f8081cc721d3bbd2b
Rob Halff <rob.halff@gmail.com>
2014-12-19 04:24:51 +0100

    sync

--- nodes/client/node.js
+++ nodes/client/node.js
@@ -1,33 +1,33 @@
-on.input.url = function(url) {
-
-  if(input.protocol) {
-    state.client = new websocket.w3cwebsocket(data, input.protocol);
-  } else {
-    state.client = new websocket.w3cwebsocket(data);
-  }
-
-  state.client.onmessage = function(event) {
-    output({ message: JSON.parse(event.data) });
-  };
-
-  state.client.onerror = function(event) {
-    output({ error: event });
-  };
-
-  state.client.onclose = function(event) {
-    output({ close: event });
-  };
-
-  state.client.onopen = function(event) {
-    output({
-      client: state.client,
-      open: event
-    });
-  };
-
-};
-
 on.input.send = function() {
+
+  if(!state.client) {
+
+    if(input.protocol) {
+      state.client = new websocket.w3cwebsocket(input.url, input.protocol);
+    } else {
+      state.client = new websocket.w3cwebsocket(input.url);
+    }
+
+    state.client.onmessage = function(event) {
+      output({ message: JSON.parse(event.data) });
+    };
+
+    state.client.onerror = function(event) {
+      output({ error: event });
+    };
+
+    state.client.onclose = function(event) {
+      output({ close: event });
+    };
+
+    state.client.onopen = function(event) {
+      output({
+        client: state.client,
+        open: event
+      });
+    };
+
+  }
 
   if(state.client && state.client.readyState === state.client.OPEN) {
     state.client.send(JSON.stringify(data));
